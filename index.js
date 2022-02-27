457a2c3a804b7d82b76fd142e8d060128749ec27
Omkar Phansopkar <omkarphansopkar@gmail.com>
2022-02-27 19:09:40 +0530

    Created APIs
    Signed-off-by: Omkar Phansopkar <omkarphansopkar@gmail.com>

--- index.js
+++ index.js
@@ -1,41 +1,172 @@
-var express = require('express'),
-    request = require('request'),
-    bodyParser = require('body-parser'),
-    app = express();
+const express = require('express');
+const slashes = require('connect-slashes');
+const cors = require('cors');
+const { default: axios } = require('axios');
 
-var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '10000kb';
-console.log('Using limit: ', myLimit);
+const PORT = process.env.PORT || 5000;
 
-app.use(bodyParser.json({limit: myLimit}));
+// Setting up the server
+const app = express();
+app.use(cors())
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true })) 
+app.use(slashes(false));
+app.listen(PORT, () => console.log(`Althsis server is up and running on port ${PORT}`));
 
-app.all('*', function (req, res, next) {
+const CLIENT_ID = "aca3456d-c1dc-4dd9-b951-297486bc447e";
+const CLIENT_SECRET = "0b07ed31-d028-4191-90d0-c4f35cab6d48";
 
-    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
-    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
-
-    if (req.method === 'OPTIONS') {
-        // CORS Preflight
-        res.send();
-    } else {
-        var targetURL = req.header('Target-URL'); // Target-URL ie. https://example.com or http://example.com
-        if (!targetURL) {
-            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
-            return;
-        }
-        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
-            function (error, response, body) {
-                if (error) {
-                    console.error('error: ' + response.statusCode)
-                }
-//                console.log(body);
-            }).pipe(res);
+app.post('/api/createConsent/:phone', (req, res) => {
+  const phone = req.params.phone;
+  if(phone.length != 10){
+    return res.json({err: "invalid phone number"});
+  }
+  axios.post(
+    'https://fiu-uat.setu.co/consents',
+    {
+      "Detail": {
+          "consentStart": `${(new Date()).toISOString()}`,
+          "consentExpiry": "2022-04-23T05:44:53.822Z",
+          "Customer": {
+              "id": `${phone}@onemoney`
+          },
+          "FIDataRange": {
+              "from": "2021-10-01T00:00:00Z",
+              "to": "2021-11-01T00:00:00Z"
+          },
+          "consentMode": "STORE",
+          "consentTypes": [
+              "TRANSACTIONS",
+              "PROFILE",
+              "SUMMARY"
+          ],
+          "fetchType": "PERIODIC",
+          "Frequency": {
+              "value": 30,
+              "unit": "MONTH"
+          },
+          "DataFilter": [
+              {
+                  "type": "TRANSACTIONAMOUNT",
+                  "value": "5000",
+                  "operator": ">="
+              }
+          ],
+          "DataLife": {
+              "value": 1,
+              "unit": "MONTH"
+          },
+          "DataConsumer": {
+              "id": "setu-fiu-id"
+          },
+          "Purpose": {
+              "Category": {
+                  "type": "string"
+              },
+              "code": "101",
+              "text": "Loan underwriting",
+              "refUri": "https://api.rebit.org.in/aa/purpose/101.xml"
+          },
+          "fiTypes": [
+              "DEPOSIT"
+          ]
+      },
+      "redirectUrl": "https://althsis.netlify.app/dash?fromSetu=true"
+    },
+    {
+      headers: {
+        "x-client-id": CLIENT_ID,
+        "x-client-secret": CLIENT_SECRET
+      }
     }
+  )
+  .then(setuResponse => {
+    res.json(setuResponse.data)
+  })
+  .catch(err => {
+    res.json({error: err})
+  });
 });
 
-app.set('port', process.env.PORT || 3000);
+app.get('/api/getConsent/:consentID', (req, res) => {
+  const consentID = req.params.consentID;
+  if(!consentID){
+    return res.json({err: "invalid consent ID"});
+  }
+  axios.get(
+    `https://fiu-uat.setu.co/consents/${consentID}`,
+    {
+      headers: {
+        "x-client-id": CLIENT_ID,
+        "x-client-secret": CLIENT_SECRET
+      }
+    }
+  )
+  .then(setuResponse => {
+    res.json(setuResponse.data)
+  })
+  .catch(err => {
+    res.json({error: err})
+  });
+});
 
-app.listen(app.get('port'), function () {
-    console.log('Proxy server listening on port ' + app.get('port'));
+app.post('/api/createDataSession/:consentID', (req, res) => {
+  const consentID = req.params.consentID;
+  if(!consentID){
+    return res.json({err: "invalid consent ID"});
+  }
+  axios.post(
+    `https://fiu-uat.setu.co/sessions`,
+    {
+      "consentId": consentID,
+      "DataRange": {
+        "from": "2021-10-01T00:00:00Z",
+        "to": "2021-11-01T00:00:00Z"
+      },
+      "format": "json"
+    },
+    {
+      headers: {
+        "x-client-id": CLIENT_ID,
+        "x-client-secret": CLIENT_SECRET
+      }
+    }
+  )
+  .then(setuResponse => {
+    res.json(setuResponse.data)
+  })
+  .catch(err => {
+    res.json({error: err})
+  });
+});
+
+app.get('/api/getData/:dataSessionID', (req, res) => {
+  const dataSessionID = req.params.dataSessionID;
+  if(!dataSessionID){
+    return res.json({err: "invalid consent ID"});
+  }
+  axios.get(
+    `https://fiu-uat.setu.co/sessions/${dataSessionID}`,
+    {
+      headers: {
+        "x-client-id": CLIENT_ID,
+        "x-client-secret": CLIENT_SECRET
+      }
+    }
+  )
+  .then(setuResponse => {
+    res.json(setuResponse.data)
+  })
+  .catch(err => {
+    res.json({error: err})
+  });
+});
+
+app.get('/responses/ping', (req, res)=>{
+    res.status(200).send('-- ok --');
+});
+
+// 404 pages for development
+app.get('*', (req, res)=>{
+    res.status(404).send("API not found :(  <br> ¯\\_(ツ)_/¯");
 });
